f67f638592a5e040019cef17d5397b3fc54e3eb8
Anuj3937 <pathakanuj2004@gmail.com>
2025-06-23 02:11:29 -0700

    queue-general

---
+++ que_ue.py
@@ -0,0 +1,24 @@
+class Queue:
+    def __init__(self,size):
+        self.items = []
+    def is_empty(self):
+        return len(self.items) == 0
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.items.pop(0)
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        return self.items[0]
+    def size(self):
+        return len(self.items)
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+print(q.dequeue())
+print(q.peek())     
+print(q.size())     
